75488f5b27462d62dadba9d62dfb1a51d0097458
Jackson J Scollan <js02777@srcs.loc>
2021-09-23 17:45:55 -0500

    Sam's Adventure Step 3: In progress

--- Level1.java
+++ Level1.java
@@ -27,9 +27,69 @@ public class Level1 extends World
     {
         setPaintOrder(Player.class, Platform.class, Obstacle.class, Collectable.class,
             Door.class, HUD.class);
+
         Door door = new Door();
-        addObject(door,1161,53);
+        addObject(door,1150,50);
+
+        SmBrickWall smbrickwall4 = new SmBrickWall();
+        addObject(smbrickwall4,1150,100);
+        
+        SmBrickWall smBrickWall5 = new SmBrickWall();
+        addObject(smBrickWall5,1050,130);
+
         Player player = new Player();
         addObject(player,41,753);
+
+        Floor floor = new Floor();
+        addObject(floor,600,800);
+
+        BrickWall brickwall = new BrickWall();
+        addObject(brickwall,650,500); 
+
+        BrickWall brickwall3 = new BrickWall();
+        addObject(brickwall3,550,150);
+
+        BrickWall brickwall2 = new BrickWall();
+        addObject(brickwall2,100,350);
+
+        TrapDoor trapdoor = new TrapDoor();
+        addObject(trapdoor,300,650); 
+
+        TrapDoor trapdoor2 = new TrapDoor();
+        addObject(trapdoor2,775,350); 
+
+        TrapDoor trapdoor3 = new TrapDoor();
+        addObject(trapdoor,700,350);
+
+        SmBrickWall smbrickwall = new SmBrickWall();
+        addObject(smbrickwall,1100,650);
+
+        SmBrickWall smbrickwall2 = new SmBrickWall();
+        addObject(smbrickwall2,1100,400);
+
+        Gem gem = new Gem();
+        addObject(gem,1100, 355);
+
+        Gem gem2 = new Gem();
+        addObject(gem2,1100, 605);
+
+        Gem gem3 = new Gem();
+        addObject(gem3,130, 305);
+        
+        SmBrickWall smbrickwall3 = new SmBrickWall();
+        addObject(smbrickwall3,850,350);
+
+        Bomb bomb = new Bomb();
+        addObject(bomb,300,765);
+
+        Bomb bomb2 = new Bomb();
+        addObject(bomb2,725,455);
+        
+        Bomb bomb3 = new Bomb();
+        addObject(bomb3,25,305);
+
+
+        
+        
     }
 }
